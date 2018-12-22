d5ff211c9e2547f3a2d8b76f34819d4411a0bbb1
ramos07 <ramosricardo96@gmail.com>
2018-12-22 13:49:13 -0800

    Update test.cpp
    added input from user (name)

--- test.cpp
+++ test.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main()
 {
+    string name;
 	cout<<"Hello this is a c++ desktop application"<<endl;
+	cout<<"Enter your name: "<<endl;
+	cin>>name;
+	cout<<name<<endl;
 
 
 	return 0;
